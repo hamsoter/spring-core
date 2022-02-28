bc25808938abcb8db7cd82fa269179a50a05f1f4
noneno <noneno@kakao.com>
2022-02-28 21:46:17 +0900

    first commit

---
+++ src/test/java/hamsoter/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hamsoter.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
