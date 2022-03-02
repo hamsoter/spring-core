0f1d2e84258f5fffad24c77b8deaf3f7b1dd3431
hamsoter <omilzmil0w0@gmail.com>
2022-03-03 01:06:46 +0900

    AppConfig 리팩토링
    MemberRepository와 DiscountPolicy도 Appconfig로 생성자를 빼주었다.
    
    이로서
    
    - 중복제거
    - AppConfig만으로도 한눈에 역할과 구현을 알아볼 수 있게되어 한번에 애플리케이션 구성을
      파악 가능

--- src/main/java/hamsoter/core/AppConfig.java
+++ src/main/java/hamsoter/core/AppConfig.java
@@ -11,11 +11,19 @@ import hamsoter.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return  new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return  new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
