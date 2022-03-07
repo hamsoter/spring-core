30bc845446c77a4a65caa9ffeedfa06bd69ff59b
hamsoter <omilzmil0w0@gmail.com>
2022-03-07 20:33:26 +0900

    옵션처리
    @Autowired 어노테이션을 사용할시, 주입할 스프링빈이 없어도
    애플리케이션이 작동되어야 할 때가 있음.
    그럴때 자동주입 대상을 옵션으로 처리해주면 빈이 null 일 때도
    애플리케이션이 터지지 않고 실행되게 해줌.

---
+++ src/test/java/hamsoter/core/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package hamsoter.core.autowired;
+
+import hamsoter.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    // 스프링 빈이 아닌 
+    static class TestBean {
+
+        // 메서드 호출 자체가 씹힘
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            System.out.println("member = " + member);
+
+        }
+
+        //
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("member = " + member);
+
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("member = " + noBean3);
+
+        }
+    }
+}
