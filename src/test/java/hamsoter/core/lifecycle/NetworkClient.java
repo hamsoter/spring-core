45d6d6b6b2a7c7cb0cac27e0e200689224fcee66
hamsoter <omilzmil0w0@gmail.com>
2022-03-09 10:11:22 +0900

    빈 설정정보에 초기화, 종료 메서드를 지정
    @Bean(initMethod = "init", destroyMEthod = "close")
    
    인터페이스 방식에 비해 훨씬 나음
    
    1. 이름 직접 지정할 수 있음
    2. 이름을 지정하지 않을시 자동으로 이름 추론해서 등록해줌
    3. 스프링 의존적이지 않음. 그저 빈 설정일 뿐이라서 ㅇㅇ

--- src/test/java/hamsoter/core/lifecycle/NetworkClient.java
+++ src/test/java/hamsoter/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hamsoter.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -30,8 +28,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 프로퍼티 세팅(의존관계 주입)이 끝나면 호출되는 메서드
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
@@ -39,8 +36,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
 
     // 빈 종료시
-    @Override
-    public void destroy() throws Exception {
+    public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
