5836c5286a37aa4015fbbf6234778823f7028417
hamsoter <omilzmil0w0@gmail.com>
2022-03-10 17:41:42 +0900

    로그를 출력하는 MyLogger 클래스 생성
    - Scope 범위를 request스코프로 지정함.
    - 생성되는 시점(HTTP 요청이 들어오는)에 고유 uuid를 생성함.
    - 소멸되는 시점(HTTP요청이 끝나는)에 종료메세지 남김.
    - requestURL은 빈 생성후 차후 setter로 입력받음.

---
+++ src/main/java/hamsoter/core/common/MyLogger.java
@@ -0,0 +1,42 @@
+package hamsoter.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    // 나중에 별도로 들어옴
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "][" + requestURL + "]" +message);
+    }
+
+    // 빈 호출과 동시에 고유 uuid 생성
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+
+
+    }
+
+    // 고객 요청이 서버에서 빠져나갈 때
+    @PreDestroy
+    public void close() {
+        System.out.println("");
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+
+    }
+}
